2eb1f624af8e37fa6278f0b62505788e93902ccf
simcap <caplette@hotmail.co.uk>
2013-03-30 20:48:48 +0100

    adding failure notification

--- lib/multicuke/runner.rb
+++ lib/multicuke/runner.rb
@@ -50,8 +50,9 @@ module Multicuke
           duration = content.match(/Finished in\s+<\w+>(.*?)</).captures.first
           scenarios = content.match(/\d+ scenarios? \((.*?)\)/).captures.first
           steps = content.match(/\d+ steps? \((.*?)\)/).captures.first
+          failed = (scenarios.include?"failed") || (steps.include?"failed")
         
-          features[dir_name] = OpenStruct.new(:scenarios => scenarios, :steps => steps, :duration => duration)
+          features[dir_name] = OpenStruct.new(:scenarios => scenarios, :steps => steps, :duration => duration, :failed? => failed)
         }
 
       }
@@ -65,11 +66,11 @@ module Multicuke
         b.body {
           b.h2("Features")
           b.ul {
-            features_dirs_to_run.each { |dir_name|
-                b.li { 
-                  b.a(dir_name, :href => "#{dir_name}.html") 
-                  b.span("[#{features[dir_name].duration}]", :class => "duration")                              
-                  b.span("Scenarios: #{features[dir_name].scenarios}, Steps: #{features[dir_name].steps}")
+            features.each { |name, feature|
+                b.li(:class => (feature.failed? ? "failed" : "success")) { 
+                  b.a(name, :href => "#{name}.html") 
+                  b.span("[#{feature.duration}]", :class => "duration")                              
+                  b.span("Scenarios: #{feature.scenarios}, Steps: #{feature.steps}", :class => "result")
                 }              
             }
           }
@@ -86,8 +87,10 @@ module Multicuke
         body {font-family: "Lucida Grande", Helvetica, sans-serif; margin: 2em 8em 2em 8em;}
         ul {list-style-type: square;} 
         li {margin: 1em 0 1em 0;}
-        li span {float: right; margin-left: 1em;}
-        .duration {font-family: ‘Lucida Console’, Monaco, monospace;}
+        li span {float: right; margin-left: 1em; padding: 0 0.3em;}
+        li.failed span.result{background: #DC6E6E;}
+        li.success span.result{background: #C1E799;}
+        span.duration {color: #999999;}
       CSS
     end
 
